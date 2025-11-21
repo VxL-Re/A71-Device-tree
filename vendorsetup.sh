52c5f22a9e0a8409c05fa0b54ec2d9339cd59e3b
VxL-Re <mahdi.r.qx@gmail.com>
2025-11-21 19:44:57 +0330

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_a71-user
-add_lunch_combo omni_a71-userdebug
-add_lunch_combo omni_a71-eng
+add_lunch_combo twrp_a71-user
+add_lunch_combo twrp_a71-userdebug
+add_lunch_combo twrp_a71-eng
